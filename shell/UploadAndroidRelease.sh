857e34aa66a0ca6b04eff15c77b7189530548839
shizhongyong <shizhongyong@wulianshuntong.com>
2019-04-30 15:03:57 +0800

    增加渠道包上传

--- shell/UploadAndroidRelease.sh
+++ shell/UploadAndroidRelease.sh
@@ -1,11 +1,42 @@
-#!/bin/bash
+#!/usr/local/opt/bash/bin/bash
+# mac系统bash版本太低，使用新版本bash
 
-./gradlew clean assembleRelease
+if [[ ${FLAVOR^^} == 'ALL' ]]; then
+	CMD='assembleRelease'
+else
+	CMD="assemble${FLAVOR^}Release"
+fi
 
-APK_FILE=$(find app/build/outputs/apk/ -name "*.apk" -type f | head -n 1)
-APK_NAME=$(basename $APK_FILE .apk)
-MAPPING_FILE=$(find app/build/outputs/mapping/ -name "mapping.txt" -type f | head -n 1)
-RELEASE_PATH=/Users/jingxin/Android/server/release
+./gradlew clean $CMD
 
-cp -f $APK_FILE $RELEASE_PATH
-cp -f $MAPPING_FILE $RELEASE_PATH/${APK_NAME}-mapping.txt
+APK_ROOT=app/build/outputs/apk
+MAPPING_ROOT=app/build/outputs/mapping
+RELEASE_ROOT=/Users/jingxin/Android/server/release
+
+for file in $APK_ROOT/*; do
+	if [[ ! -d $file ]]; then
+		continue
+	fi
+
+	CHANNEL=$(basename $file)
+	APK_FILE=$(find $file -name "*.apk" -type f | head -n 1)
+	MAPPING_FILE=$(find $MAPPING_ROOT/$CHANNEL -name "mapping.txt" -type f | head -n 1)
+	
+	if [[ ! $APK_FILE ]]; then
+		continue
+	fi
+
+	APP_ID=$(apkanalyzer manifest application-id $APK_FILE)
+	RELEASE_PATH=$RELEASE_ROOT/$APP_ID/$CHANNEL
+
+	if [[ ! -d $RELEASE_PATH ]]; then
+		mkdir -p $RELEASE_PATH
+	fi
+	
+	cp -f $APK_FILE $RELEASE_PATH
+
+	if [[ $MAPPING_FILE ]]; then
+		APK_NAME=$(basename $APK_FILE .apk)
+		cp -f $MAPPING_FILE $RELEASE_PATH/${APK_NAME}-mapping.txt
+	fi
+done
