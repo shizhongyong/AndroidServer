540f261a3afdc4a431800b4d399c6910dc484674
shizhongyong <shizhongyong@wulianshuntong.com>
2021-04-23 18:12:58 +0800

    modify shell

--- shell/UploadAndroidRelease.sh
+++ shell/UploadAndroidRelease.sh
@@ -7,7 +7,7 @@ else
 	CMD="assemble${FLAVOR^}Release"
 fi
 
-./gradlew clean $CMD
+./gradlew clean $CMD -PenableAbiSplit
 
 # $?: 上个命令的退出状态，或函数的返回值
 ret=$?
@@ -23,13 +23,13 @@ APK_ROOT=app/build/outputs/apk
 MAPPING_ROOT=app/build/outputs/mapping
 RELEASE_ROOT=/Users/jingxin/Android/server/release
 
-for file in $APK_ROOT/*; do
-	if [[ ! -d $file ]]; then
+for flavorPath in $APK_ROOT/*; do
+	if [[ ! -d $flavorPath ]]; then
 		continue
 	fi
 
-	CHANNEL=$(basename $file)
-	APK_FILE=$(find $file -name "*.apk" -type f | head -n 1)
+	CHANNEL=$(basename $flavorPath)
+	APK_FILE=$(find $flavorPath -name "*.apk" -type f | head -n 1)
 	MAPPING_FILE=$(find $MAPPING_ROOT/$CHANNEL -name "mapping.txt" -type f | head -n 1)
 	
 	if [[ ! $APK_FILE ]]; then
@@ -37,7 +37,7 @@ for file in $APK_ROOT/*; do
 	fi
 
 	if [[ ! $MAPPING_FILE ]]; then
-		#从build tools 3.6.0开始，mapping目录有变化。便如：mapping/officialRelease
+		#从build tools 3.6.0开始，mapping目录有变化。例如：mapping/officialRelease
 	    MAPPING_FILE=$(find $MAPPING_ROOT/${CHANNEL}Release -name "mapping.txt" -type f | head -n 1)
 	fi
 
@@ -48,10 +48,11 @@ for file in $APK_ROOT/*; do
 		mkdir -p $RELEASE_PATH
 	fi
 	
-	cp -f $APK_FILE $RELEASE_PATH
+	find $flavorPath -name "*.apk" -type f -print | xargs -I {} cp -f {} $RELEASE_PATH
 
 	if [[ $MAPPING_FILE ]]; then
-		APK_NAME=$(basename $APK_FILE .apk)
-		cp -f $MAPPING_FILE $RELEASE_PATH/${APK_NAME}-mapping.txt
+		APK_NAME=$(basename $APK_FILE)
+		MAPPING_NAME_PREFIX=${APK_NAME%${CHANNEL}*}
+		cp -f $MAPPING_FILE $RELEASE_PATH/${MAPPING_NAME_PREFIX}${CHANNEL}-mapping.txt
 	fi
 done
